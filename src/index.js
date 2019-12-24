b7730b7726255126e311b897ee24ec7eba761e20
Aditya Nagla <me@cdadityang.xyz>
2019-12-24 07:21:07 +0000

    V1.0.7: Added analytics integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Router, BrowserRouter } from 'react-router-dom';
+import history from './history/history';
+import PiwikReactRouter from 'piwik-react-router';
+
 import App from './App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+if(process.env.NODE_ENV === "production"){
+  // Matomo/Piwik Setup
+  const piwik = PiwikReactRouter({
+    url: 'https://analytics.cdadityang.xyz',
+    siteId: 3
+  });
+
+  ReactDOM.render(
+    <Router history={piwik.connectToHistory(history)}>
+      <App />
+    </Router>,
+    document.getElementById('root')
+  );
+} else{
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
+}
